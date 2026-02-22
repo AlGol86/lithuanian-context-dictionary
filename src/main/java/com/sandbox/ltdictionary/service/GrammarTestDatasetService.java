9a80db49dd3c3ee8693c39794cab25c63fb583d1
Aleksei Golovenchik <aleksei.golovenchik@linqo.eu>
2026-02-22 17:34:21 +0200

    add 3-d vocabulary part

--- src/main/java/com/sandbox/ltdictionary/service/GrammarTestDatasetService.java
+++ src/main/java/com/sandbox/ltdictionary/service/GrammarTestDatasetService.java
@@ -6,17 +6,16 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import tools.jackson.core.JacksonException;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -28,16 +27,10 @@ public class GrammarTestDatasetService {
 
     private final ObjectMapper objectMapper;
     private final String absPathToResults;
-    private final String absPathToWordTranslationsPrefix;
 
-    public GrammarTestDatasetService(
-        @Value("${grammar-tests-results.absolute-path}") String absPathToResults,
-        @Value("${word-translations-prefix.absolute-path}") String absPathToWordTranslationsPrefix,
-        ObjectMapper objectMapper
-    ) {
+    public GrammarTestDatasetService(@Value("${grammar-tests-results.absolute-path}") String absPathToResults, ObjectMapper objectMapper) {
         this.absPathToResults = absPathToResults;
         this.objectMapper = objectMapper;
-        this.absPathToWordTranslationsPrefix = absPathToWordTranslationsPrefix;
     }
 
     @SneakyThrows
@@ -58,12 +51,12 @@ public class GrammarTestDatasetService {
     @SneakyThrows
     public List<WordTranslation> loadWords(String chapterName) {
         AtomicInteger cntr = new AtomicInteger(0);
-        return Files.readAllLines(Path.of(absPathToWordTranslationsPrefix + chapterName + ".txt")).stream().filter(Predicate.not(String::isBlank)).map(line -> {
-                    String[] elements = line.split(",");
-                    if (elements.length < 2) return null;
-                    if ((elements.length > 2)) return new WordTranslation(cntr.getAndIncrement(), elements[0].trim(), elements[1].trim(), elements[2].trim());
-                    return new WordTranslation(cntr.getAndIncrement(), elements[0], elements[1], null);
-        }).toList();
+        return Files.readAllLines(Path.of(GrammarTestDatasetService.class.getResource("/text-materials-for-tasks/" + chapterName + ".txt").toURI())).stream().filter(Predicate.not(String::isBlank)).map(line -> {
+            String[] elements = line.split(",");
+            if (elements.length < 2) return null;
+            if ((elements.length > 2)) return new WordTranslation(cntr.getAndIncrement(), elements[0].trim(), elements[1].trim(), elements[2].trim());
+            return new WordTranslation(cntr.getAndIncrement(), elements[0], elements[1], null);
+        }).filter(Objects::nonNull).toList();
     }
 
 }
